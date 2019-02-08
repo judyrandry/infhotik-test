74dedae4069391fa712accf2f245597ae6a5863a
judicael <judicael.r@m-ite.com>
2019-02-08 16:30:30 +0300

    inject ApiService in the product listing component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../api.service";
 
 @Component({
   selector: 'app-product',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  products: any;
 
-  constructor() { }
+  constructor(private api: ApiService) {
+  }
 
   ngOnInit() {
   }
