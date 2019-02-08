640ec3f6f1edfb430e35eba3e2535a31a256ead2
judicael <judicael.r@m-ite.com>
2019-02-08 16:14:37 +0300

    inject the httpClient module in the api sevice

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -11,6 +12,6 @@ const apiUrl = "/api";
 })
 export class ApiService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 }
