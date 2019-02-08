952bb825bd34376aecae352532595a463d28b952
judicael <judicael.r@m-ite.com>
2019-02-08 15:56:18 +0300

    create routes in the front angular | Products LIst and Product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProductComponent} from "./product/product.component";
+import {ProductDetailComponent} from "./product-detail/product-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    data: { title: 'Products List' }
+  },
+  {
+    path: 'Product-details/:id',
+    component: ProductDetailComponent,
+    data: { title: 'Product Details' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
