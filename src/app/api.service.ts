2a069919ad2a1b343b5a6fb39546052a4b7dd64f
judicael <judicael.r@m-ite.com>
2019-02-08 16:12:41 +0300

    define request headers and the apiUrl

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpHeaders} from "@angular/common/http";
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = "/api";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor() {
+  }
 }
